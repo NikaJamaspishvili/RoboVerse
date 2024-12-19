a667e6018d616e3c43fa0ad0f083d2ea6a0ce004
macbook <jamaspishvilinika@icloud.com>
2024-12-19 21:20:25 +0400

    resolved most of the issue with rendering of the animated dom

--- src/model/Model.jsx
+++ src/model/Model.jsx
@@ -2,18 +2,18 @@ import "./model.css";
 
 import { useParams } from "react-router-dom";
 
-import {Canvas,} from "@react-three/fiber";
-import {OrbitControls,Html,useProgress,Environment, useFBO} from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls,Html,Environment} from "@react-three/drei";
 
-import { motion } from "framer-motion";
+import { motion,AnimatePresence } from "framer-motion";
 
-import { lazy, Suspense,useState,useEffect } from "react";
-import { Scene } from "three";
+import { lazy, Suspense,useState,useEffect,useRef } from "react";
 
-const Companion = lazy(()=>(import('../../public/robot_companion/Companion.jsx')));
+import Companion from "../../public/robot_companion/Companion.jsx";
 const Household = lazy(()=>(import('../../public/robot_household/Household.jsx')));
 const Security = lazy(()=>(import('../../public/robot_security/Security.jsx')));
 const Education = lazy(()=>(import('../../public/robot_education/Education.jsx')));
+
 
 function Model(){
   const {type} = useParams();
@@ -22,7 +22,7 @@ function Model(){
   const [btnValue,setBtnValue] = useState("text");
   const [seconds,setSeconds] = useState(0);
   const [btnClick,setBtnClick] = useState(false);
-  const [isMounted,setIsMounted] = useState(false);
+  const [isModelLoaded,setIsModelLoaded] = useState(true);
 
   const count = 7;
 
@@ -46,10 +46,6 @@ function Model(){
   
   }, [seconds,btnClick]);
 
-  useEffect(() => {
-    setIsMounted(true);
-    return () => setIsMounted(false);
-  }, []);
 
   function handleClick(){
     if(btnValue === 'text'){
@@ -63,18 +59,18 @@ let parentVariants = {
     hidden:{
       opacity:0,
     },
-    show:{
+    visible:{
       opacity:1,
       transition:{
         duration:2,
         staggerChildren:0.2
       }
-    }
+    },
   }
 
   let childrenVariants = {
     hidden:{opacity:0},
-    show:{opacity:1}
+    visible:{opacity:1}
   }
 
   const array = [
@@ -88,42 +84,48 @@ let parentVariants = {
 
  if(target){
   return <div className="modelWrapper">
-   <button id="backButton"><i class="fa-solid fa-arrow-left" onClick={()=>{window.history.back()}}></i></button>
-  <Canvas id="modelWrapperCanvas" style={{height:"70svh",maxWidth:"450px"}} camera={{ position: [4,4,4], fov: 75 }}>
-   <OrbitControls/>
-   <Environment preset="dawn"/>
-    <Suspense fallback={<Html><div><i class="fa-solid fa-spinner"></i></div></Html>}>
-    {type === "companion" && <Companion scale={[1.5, 1.5, 1.5]} />}
-    {type === "security" && <Security scale={[1.5, 1.5, 1.5]} />}
-    {type === "household" && <Household scale={[2, 2, 2]} />}
-    {type === "education" && <Education scale={[0.2,0.2,0.2]} />}   
-    </Suspense>
-  </Canvas>
-
-  {isMounted && <motion.section key={type} variants={parentVariants} initial="hidden" animate="show">
-   
-   <motion.div className="productHeader">
-    <motion.h1 variants={childrenVariants}>{target.name.toUpperCase()}</motion.h1>
-    <motion.h2 variants={childrenVariants}><span>By</span> RoboVerse</motion.h2>
-   </motion.div>
-    <motion.h3 variants={childrenVariants}>${target.price}</motion.h3>
-
-    <motion.p variants={childrenVariants}>{target.description}</motion.p>
-    
-    <motion.div  variants={childrenVariants} className="quantityButton">
-    <p>Quantity</p>
-    <div>
-      <button onClick={()=>{quantity !== 1 && setQuantity(quantity-1)}}><i class="fa-solid fa-minus"></i></button>
-      <p>{quantity}</p>
-      <button onClick={()=>{setQuantity(quantity+1)}}><i class="fa-solid fa-plus"></i></button>
-      </div>
-    </motion.div>
-
-    <motion.button style={{animation:btnValue !== "text" && "btnAnimation "+count+"s"}} onClick={handleClick} variants={childrenVariants}>{btnValue === "text" ? "BUY NOW":btnValue === "checkmark" && <i class="fa-solid fa-check"></i>}</motion.button>
-  </motion.section>
-  }
-
+  <button id="backButton">
+    <i className="fa-solid fa-arrow-left" onClick={() => { window.history.back() }}></i>
+  </button>
+  
+    <Canvas id="modelWrapperCanvas" style={{ height: "70svh", maxWidth: "450px" }} camera={{ position: [4, 4, 4], fov: 75 }}>
+      <OrbitControls />
+      <Environment preset="dawn" />
+      <Suspense fallback={<Html><div><i className="fa-solid fa-spinner"></i></div></Html>}>
+        {type === "companion" && <Companion scale={[1.5, 1.5, 1.5]} />}
+        {type === "security" && <Security scale={[1.5, 1.5, 1.5]} />}
+        {type === "household" && <Household scale={[2, 2, 2]} />}
+        {type === "education" && <Education scale={[0.2, 0.2, 0.2]} />}
+      </Suspense>
+    </Canvas>
+  
+  <AnimatePresence mode="wait">
+    <motion.section key={type} variants={parentVariants} initial="hidden" animate="visible">
+      <motion.div className="productHeader">
+        <motion.h1 variants={childrenVariants}>{target.name.toUpperCase()}</motion.h1>
+        <motion.h2 variants={childrenVariants}><span>By</span> RoboVerse</motion.h2>
+      </motion.div>
+      <motion.h3 variants={childrenVariants}>${target.price}</motion.h3>
+      <motion.p variants={childrenVariants}>{target.description}</motion.p>
+      <motion.div variants={childrenVariants} className="quantityButton">
+        <p>Quantity</p>
+        <div>
+          <button onClick={() => { quantity !== 1 && setQuantity(quantity - 1) }}>
+            <i className="fa-solid fa-minus"></i>
+          </button>
+          <p>{quantity}</p>
+          <button onClick={() => { setQuantity(quantity + 1) }}>
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        </div>
+      </motion.div>
+      <motion.button style={{ animation: btnValue !== "text" && "btnAnimation " + count + "s" }} onClick={handleClick} variants={childrenVariants}>
+        {btnValue === "text" ? "BUY NOW" : btnValue === "checkmark" && <i className="fa-solid fa-check"></i>}
+      </motion.button>
+    </motion.section>
+  </AnimatePresence>
 </div>
+
  }
 }
 
